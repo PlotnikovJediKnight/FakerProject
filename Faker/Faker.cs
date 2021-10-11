47072508a626800ca89b650e6c3691235dcb68a3
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-10-11 08:54:20 +0300

    Добавьте файлы проекта.

---
+++ Faker/Faker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    class Faker
+    {
+        public T Create<T>()
+        {    return (T) Create(typeof(T));}
+        
+        private object Create(Type t)
+        {
+            return null;
+        }
+    }
+}
